2833622968f20e378b6c3890c6aaf1425e2fdf7a
OlgaOtasevic <miezemause@gmail.com>
2023-10-02 19:08:56 +0200

    Initial commit

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,51 @@
+package Pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    WebElement usernameField;
+    WebElement passwordField;
+    WebElement submitButton;
+    WebElement error;
+
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public WebElement getUsernameField() {
+        return driver.findElement(By.id("username"));
+    }
+
+    public WebElement getPasswordField() {
+        return driver.findElement(By.id("password"));
+    }
+
+    public WebElement getSubmitButton() {
+        return driver.findElement(By.id("submit"));
+    }
+
+    public WebElement getError() {
+        return driver.findElement(By.id("error"));
+    }
+
+    //--------------------------
+
+    public void inputUsername(String username) {
+        getUsernameField().clear();
+        getUsernameField().sendKeys(username);
+    }
+
+    public void inputPassword(String password) {
+        getPasswordField().clear();
+        getPasswordField().sendKeys(password);
+    }
+
+    public void clickOnSubmit() {
+        getSubmitButton().click();
+    }
+}
---
+++ src/test/java/Pages/ProfilePage.java
@@ -0,0 +1,35 @@
+package Pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProfilePage {
+    public WebDriver driver;
+    public WebDriverWait wait;
+    WebElement message;
+    WebElement logoutButton;
+
+    public ProfilePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public WebElement getMessage() {
+        return driver.findElement(By.className("has-text-align-center"));
+    }
+
+    public WebElement getLogoutButton() {
+        return driver.findElement(By.linkText("Log out"));
+    }
+
+    //--------------------------
+
+    public void clickOnLogoutButton() {
+        getLogoutButton().click();
+    }
+
+    public String messageText() {
+        return getMessage().getText();
+    }
+}
